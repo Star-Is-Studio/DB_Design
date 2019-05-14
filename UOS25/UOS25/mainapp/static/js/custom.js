22d44715b41dd8b930fe72baf3008cb8295fb65f
jeonghan <qlqhqo2341@naver.com>
2019-05-14 17:13:16 +0900

    화면 리사이징 버그를 0.1초 타임아웃추가로 해결.

--- UOS25/UOS25/mainapp/static/js/custom.js
+++ UOS25/UOS25/mainapp/static/js/custom.js
@@ -1,14 +1,17 @@
 $(window).resize(function(){
-    var windowWidth = window.outerWidth;
-    console.log(windowWidth);
-    if(windowWidth < 900){
-        $("#sdBar").addClass("nav nav-pills nav-justified");
-        $("#sdBar").removeClass("sideBar side-bar-small");
-        $('.main-small').css("margin","0");
-    }
-    else{
-        $("#sdBar").addClass("sideBar side-bar-small");
-        $("#sdBar").removeClass("nav nav-pills nav-justified");
-        $('.main-small').css("margin-left","250px");
-    }
+    setTimeout(function(){
+        var windowWidth = window.outerWidth;
+        console.log(windowWidth);
+        if(windowWidth < 900){
+            $("#sdBar").addClass("nav nav-pills nav-justified");
+            $("#sdBar").removeClass("sideBar side-bar-small");
+            $('.main-small').css("margin","0");
+        }
+        else{
+            $("#sdBar").addClass("sideBar side-bar-small");
+            $("#sdBar").removeClass("nav nav-pills nav-justified");
+            $('.main-small').css("margin-left","250px");
+        }
+
+    },100);
 });
