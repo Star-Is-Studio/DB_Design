b80dd6d5740e8171b82efd3fe7ae21372ce225f2
jeonghan <qlqhqo2341@naver.com>
2019-05-16 21:00:09 +0900

    페이지 url을 urls.py의 함수 이름에 바인딩 되도록 변경 본사페이지 로고링크를 본사페이지로 이동하도록 변경

--- UOS25/UOS25/UOS25/urls.py
+++ UOS25/UOS25/UOS25/urls.py
@@ -21,29 +21,29 @@ from mainapp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',login),
-    path('',login),#로그인페이지
-    path('index/',index),#가맹점인덱스
-    path('indexAdmin/',indexAdmin),#본사인덱스
+    path('',login, name='login'),#로그인페이지
+    path('index/',index, name='index'),#가맹점인덱스
+    path('indexAdmin/',indexAdmin, name='indexAdmin'),#본사인덱스
     ###가맹점페이지
-    path('orderHeadManage/',orderHeadManage),#본사주문관리
-    path('refundHeadManage/',refundHeadManage),#본사반품관리
-    path('saleProduct/',saleProduct),#물품판매
-    path('refundCustomerManage/',refundCustomerManage),#고객반품관리
-    path('registerProduct/',registerProduct),#재고추가
-    path('deleteProduct/',deleteProduct),#재고삭제
-    path('expiryDateManage/',expiryDateManage),#유통기한관리123
-    path('saleManage/',saleManage),#매출관리123
-    path('costManage/',costManage),#유지비관리123
-    path('employeeManage/',employeeManage),#점원관리
-    path('workRecordManage/',workRecordManage),#근무기록관리123
-    path('salaryManage/',salaryManage),#월급관리123
+    path('orderHeadManage/',orderHeadManage, name='orderHeadManage'),#본사주문관리
+    path('refundHeadManage/',refundHeadManage, name='refundHeadManage'),#본사반품관리
+    path('saleProduct/',saleProduct, name='saleProduct'),#물품판매
+    path('refundCustomerManage/',refundCustomerManage, name='refundCustomerManage'),#고객반품관리
+    path('registerProduct/',registerProduct, name='registerProduct'),#재고추가
+    path('deleteProduct/',deleteProduct, name='deleteProduct'),#재고삭제
+    path('expiryDateManage/',expiryDateManage, name='expiryDateManage'),#유통기한관리123
+    path('saleManage/',saleManage, name='saleManage'),#매출관리123
+    path('costManage/',costManage, name='costManage'),#유지비관리123
+    path('employeeManage/',employeeManage, name='employeeManage'),#점원관리
+    path('workRecordManage/',workRecordManage, name='workRecordManage'),#근무기록관리123
+    path('salaryManage/',salaryManage, name='salaryManage'),#월급관리123
     ###본사페이지
-    path('franchiseManage/',franchiseManage),#가맹점 관리
-    path('franchiseCostManage/',franchiseCostManage),#가맹요금 수납
-    path('deliveryManage/',deliveryManage),#납품 업체 관리
-    path('franchiseOrderManage/',franchiseOrderManage),#가맹점 주문 관리
-    path('registerHeadProduct/',registerHeadProduct),#상품 등록
-    path('HeadProductManage/',HeadProductManage),#상품 조회/삭제
-    path('registerCustomer/',registerCustomer),#고객 등록
-    path('customerManage/',customerManage),#고객 조회/삭제
+    path('franchiseManage/',franchiseManage, name='franchiseManage'),#가맹점 관리
+    path('franchiseCostManage/',franchiseCostManage, name='franchiseCostManage'),#가맹요금 수납
+    path('deliveryManage/',deliveryManage, name='deliveryManage'),#납품 업체 관리
+    path('franchiseOrderManage/',franchiseOrderManage, name='franchiseOrderManage'),#가맹점 주문 관리
+    path('registerHeadProduct/',registerHeadProduct, name='registerHeadProduct'),#상품 등록
+    path('HeadProductManage/',HeadProductManage, name='HeadProductManage'),#상품 조회/삭제
+    path('registerCustomer/',registerCustomer, name='registerCustomer'),#고객 등록
+    path('customerManage/',customerManage, name='customerManage'),#고객 조회/삭제
 ]
