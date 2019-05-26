38b6cdedeb864c487dd73497a163f2e7f7652da4
jeonghan <qlqhqo2341@naver.com>
2019-05-27 00:57:46 +0900

    장고 실행을 못하는 버그수정.

--- UOS25/UOS25/mainapp/models.py
+++ UOS25/UOS25/mainapp/models.py
@@ -19,27 +19,27 @@ class Product(models.Model):
     content = models.CharField(max_length=50)
     supply_price = models.IntegerField()
     unit_price = models.IntegerField()
-    supplier_id = models.ForeignKey(Supplier.id) # ?
+    supplier_id = models.ForeignKey(Supplier) # ?
     category_a = models.IntegerField() # 상품 대분류 코드
     category_b = models.IntegerField() # 상품 소분류 코드
     information = models.TextField()
     img_path = models.TextField() # 사진파일경로
 
 class Stock(models.Model):
-    store_id = models.ForeignKey(Store.id)
+    store_id = models.ForeignKey(Store)
     location_code = models.IntegerField() # TODO 이름이..
-    barcode  = models.ForeignKey(Product.barcode)
+    barcode  = models.ForeignKey(Product)
     quantity = models.IntegerField()
 
 class Order(models.Model):
-    store_id = models.ForeignKey(Store.id)
+    store_id = models.ForeignKey(Store)
     order_timestamp = models.DateTimeField()
     complete_timestamp = models.DateTimeField()
     process_code = models.IntegerField()
     
 class Orderlist(models.Model):
-    order_id = models.ForeignKey(Order.id)
-    barcode = models.ForeignKey(Product.barcode)
+    order_id = models.ForeignKey(Order)
+    barcode = models.ForeignKey(Product)
     quantity = models.IntegerField()
     sent_timestamp = models.DateTimeField()
     arrival_timestamp = models.DateTimeField()
@@ -47,62 +47,62 @@ class Orderlist(models.Model):
     
 
 class Storerefund(models.Model):
-    store_id = models.ForeignKey(Store.id)
-    barcode = models.ForeignKey(Product.id)
+    store_id = models.ForeignKey(Store)
+    barcode = models.ForeignKey(Product)
     quantity = models.IntegerField()
     refund_timestamp = models.IntegerField()
     refund_reason_code = models.IntegerField()
     process_code = models.IntegerField()
 
 class Storercpt(models.Model): # 가맹요금수납
-    store_id = models.ForeignKey(Store.id)
+    store_id = models.ForeignKey(Store)
     rcpt_timestamp = models.DateTimeField()
     amount = models.IntegerField()
     
 class Employee(models.Model):
-    store_id = models.ForeignKey(Store.id)
+    store_id = models.ForeignKey(Store)
     name = models.CharField(max_length=25)
     daytime_hourpay = models.IntegerField()
     nighttime_hourpay = models.IntegerField()
     employed_date =  models.DateField()
     fire_date=  models.DateField()
-    contact = models.CharField(20)
+    contact = models.CharField(max_length=25)
     position_code = models.IntegerField()
 
 class Worklist(models.Model):
-    emp_id = models.ForeignKey(Employee.id)
+    emp_id = models.ForeignKey(Employee)
     name = models.CharField(max_length=25)
     workstart_timestamp = models.DateTimeField()
     workend_timestamp = models.DateTimeField()
     storeowner_check = models.BooleanField()
 
 class Maintenance(models.Model):
-    store_id = models.ForeignKey(Store.id)
+    store_id = models.ForeignKey(Store)
     maintenance_code = models.IntegerField()
     amount = models.IntegerField()
     process_date = models.DateField()
-    emp_id = models.ForeignKey(Employee.id)
+    emp_id = models.ForeignKey(Employee)
     information = models.TextField() # 비고
     storeowner_check = models.BooleanField()
 
 class Customer(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=40)
     mileage = models.IntegerField()
     gender = models.IntegerField()
     birthday = models.DateField()
-    contact = models.CharField()
+    contact = models.CharField(max_length=20)
 
 class Receipt(models.Model):
-    store_id = models.ForeignKey(Store.id)
+    store_id = models.ForeignKey(Store)
     trade_timestamp = models.DateTimeField()
-    emp_id = models.ForeignKey(Employee.id)
-    customer_id = models.ForeignKey(Customer.id)
+    emp_id = models.ForeignKey(Employee)
+    customer_id = models.ForeignKey(Customer)
     payment_method_code = models.IntegerField()
     payment_information = models.CharField(max_length=20) # TODO 초단문?
     
 class Tradelist(models.Model):
-    receipt_id = models.ForeignKey(Receipt.id)
-    barcode = models.ForeignKey(Product.barcode)
+    receipt_id = models.ForeignKey(Receipt)
+    barcode = models.ForeignKey(Product)
     quantity = models.IntegerField()
     is_refund = models.BooleanField()
     
@@ -117,3 +117,7 @@ class Setting(models.Model):
 
 class Code(models.Model):
     information = models.CharField(max_length=20)
+
+# no need.
+class User(models.Model):
+    userId = models.IntegerField()
