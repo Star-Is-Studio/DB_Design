7dc1a2033b7e6943ef6c3ce8d5ed2be3e5f5b114
jeonghan <qlqhqo2341@naver.com>
2019-06-05 13:41:14 +0900

    샘플데이터 및 url구조 변경

--- UOS25/UOS25/UOS25/urls.py
+++ UOS25/UOS25/UOS25/urls.py
@@ -17,33 +17,39 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from mainapp.views import *
 from django.conf.urls import url as path
-#123 : 미구현
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/',login),
-    path(r'^$',login, name='login'),#로그인페이지
-    path('index/',index, name='index'),#가맹점인덱스
-    path('indexAdmin/',indexAdmin, name='indexAdmin'),#본사인덱스
-    ###가맹점페이지
-    path('orderHeadManage/',orderHeadManage, name='orderHeadManage'),#본사주문관리
-    path('refundHeadManage/',refundHeadManage, name='refundHeadManage'),#본사반품관리
-    path('saleProduct/',saleProduct, name='saleProduct'),#물품판매
-    path('refundCustomerManage/',refundCustomerManage, name='refundCustomerManage'),#고객반품관리
-    path('registerProduct/',registerProduct, name='registerProduct'),#재고추가
-    path('deleteProduct/',deleteProduct, name='deleteProduct'),#재고삭제
-    path('expiryDateManage/',expiryDateManage, name='expiryDateManage'),#유통기한관리123
-    path('saleManage/',saleManage, name='saleManage'),#매출관리123
-    path('costManage/',costManage, name='costManage'),#유지비관리123
-    path('employeeManage/',employeeManage, name='employeeManage'),#점원관리
-    path('workRecordManage/',workRecordManage, name='workRecordManage'),#근무기록관리123
-    path('salaryManage/',salaryManage, name='salaryManage'),#월급관리123
-    ###본사페이지
-    path('franchiseManage/',franchiseManage, name='franchiseManage'),#가맹점 관리
-    path('franchiseCostManage/',franchiseCostManage, name='franchiseCostManage'),#가맹요금 수납
-    path('deliveryManage/',deliveryManage, name='deliveryManage'),#납품 업체 관리
-    path('franchiseOrderManage/',franchiseOrderManage, name='franchiseOrderManage'),#가맹점 주문 관리
-    path('registerHeadProduct/',registerHeadProduct, name='registerHeadProduct'),#상품 등록
-    path('HeadProductManage/',HeadProductManage, name='HeadProductManage'),#상품 조회/삭제
-    path('registerCustomer/',registerCustomer, name='registerCustomer'),#고객 등록
-    path('customerManage/',customerManage, name='customerManage'),#고객 조회/삭제
+
+# 본사 페이지
+admin_urlpatterns = [
+    path(r'^admin/$',indexAdmin, name='indexAdmin'),#본사인덱스
+    path(r'^admin/ranchiseManage/$',franchiseManage, name='franchiseManage'),#가맹점 관리
+    path(r'^admin/franchiseCostManage/$',franchiseCostManage, name='franchiseCostManage'),#가맹요금 수납
+    path(r'^admin/deliveryManage/$',deliveryManage, name='deliveryManage'),#납품 업체 관리
+    path(r'^admin/franchiseOrderManage/$',franchiseOrderManage, name='franchiseOrderManage'),#가맹점 주문 관리
+    path(r'^admin/registerHeadProduct/$',registerHeadProduct, name='registerHeadProduct'),#상품 등록
+    path(r'^admin/HeadProductManage/$',HeadProductManage, name='HeadProductManage'),#상품 조회/삭제
+    path(r'^admin/registerCustomer/$',registerCustomer, name='registerCustomer'),#고객 등록
+    path(r'^admin/customerManage/$',customerManage, name='customerManage'),#고객 조회/삭제
 ]
+
+# 가맹점 페이지
+franchise_urlpatterns = [    
+    path(r'^franchise/$',index, name='index'),#가맹점인덱스
+    path(r'^franchise/orderHeadManage/$',orderHeadManage, name='orderHeadManage'),#본사주문관리
+    path(r'^franchise/refundHeadManage/$',refundHeadManage, name='refundHeadManage'),#본사반품관리
+    path(r'^franchise/saleProduct/$',saleProduct, name='saleProduct'),#물품판매
+    path(r'^franchise/refundCustomerManage/$',refundCustomerManage, name='refundCustomerManage'),#고객반품관리
+    path(r'^franchise/registerProduct/$',registerProduct, name='registerProduct'),#재고추가
+    path(r'^franchise/deleteProduct/$',deleteProduct, name='deleteProduct'),#재고삭제
+    path(r'^franchise/expiryDateManage/$',expiryDateManage, name='expiryDateManage'),#유통기한관리
+    path(r'^franchise/saleManage/$',saleManage, name='saleManage'),#매출관리
+    path(r'^franchise/costManage/$',costManage, name='costManage'),#유지비관리
+    path(r'^franchise/employeeManage/$',employeeManage, name='employeeManage'),#점원관리
+    path(r'^franchise/workRecordManage/$',workRecordManage, name='workRecordManage'),#근무기록관리
+    path(r'^franchise/salaryManage/$',salaryManage, name='salaryManage'),#월급관리
+]
+
+# 통합페이지
+urlpatterns = [
+    path(r'^$',login, name='login'),#로그인페이지
+] + admin_urlpatterns + franchise_urlpatterns
+
--- UOS25/UOS25/mainapp/query.py
+++ UOS25/UOS25/mainapp/query.py
@@ -1,4 +1,13 @@
 from models import *
 
 class ManageStore():
+    pass
+
+class ManageCenter():
+    def store(method, value=None, original_value=None):
+        '''
+        method ; get, modify, put
+        value ; modify, put
+        original_value ; get, modify
+        '''
     
