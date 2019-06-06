0c9f71067182113b371e5386d5c26c40c5f35904
jeonghan <qlqhqo2341@naver.com>
2019-06-06 09:18:19 +0900

    query_insert 구현

--- UOS25/UOS25/mainapp/query.py
+++ UOS25/UOS25/mainapp/query.py
@@ -1,4 +1,5 @@
 from mainapp.models import *
+from django.db import connection
 
 def query_all(model):
     sql = 'select * from {}'
@@ -11,3 +12,24 @@ def query_pk(model, pk, pk_name='id'):
     return model.objects.raw(
         sql.format(table_name, pk_name, pk)
     )
+
+def query_insert(model, data : dict):
+    '''
+    data : 딕셔너리
+        key:필드이름, value:필드값
+    '''
+    sql_base = 'insert into {}({}) values ({})'
+    table_name = model._meta.db_table
+    def sql_convert(obj):
+        sql_type_convert = {
+            str: lambda x : "'{}'".format(x),
+            int: lambda x : str(x),
+            float: lambda x : str(x),
+        }
+        return sql_type_convert[ type(obj) ](obj)
+    keys = list(data.keys())
+    fields_str = ",".join(keys)
+    fields_value_str = ",".join([ sql_convert(data[k]) for k in keys])
+    sql = sql_base.format(table_name, fields_str, fields_value_str)
+    with connection.cursor() as cur:
+        cur.execute(sql)
