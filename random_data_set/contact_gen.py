fbe5a561c39be980f135f32608f3b1ec20045788
KimHyunwoo <41246591+diablo8273@users.noreply.github.com>
2019-06-08 02:10:05 +0900

    .

--- random_data_set/contact_gen.py
+++
@@ -1,6 +0,0 @@
-import random
-r = random.randint
-cases = int(input("how many generate contacts? : "))
-with open("random_data_set/contact.csv","wt") as f:
-    for _ in range(cases):
-        print("010-{:04d}-{:04d}".format(r(0,9999),r(0,9999)), file=f)
--- random_data_set/email_gen.py
+++
@@ -1,8 +0,0 @@
-import random
-with open('random_data_set/english_word.csv','rt') as f:
-    with open("random_data_set/email.csv","wt") as wf:
-        words = f.readlines()
-        counts = int(input("How many generate email? : "))
-        for _ in range(counts):
-            id,domain = random.choices(words,k=2)
-            print("{}@{}.com".format(id.strip(),domain.strip()),file=wf)
